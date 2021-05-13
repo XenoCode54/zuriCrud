3bd06f92c6af11193338478e69cb3148debcb995
{xenon} <{macolawale@gmail.com}>
2021-05-13 16:25:44 +0530

    commit

--- index.js
+++ index.js
@@ -5,16 +5,14 @@ const bodyParser = require("body-parser");
 const app = express();
 
 const port = process.env.PORT || 3000
-const MONGODB_URL = "mongodb+srv://macxenonzuri:macxenonzuri123@cluster0.dwd2i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
-console.log(process.env.MONGODB_URI)
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
 
 // parse application/json
 app.use(bodyParser.json())
 
-mongoose.connect(process.env.MONGODB_URI || MONGODB_URL || 'mongodb://localhost/zuriCRUD', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/zuriCRUD', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
